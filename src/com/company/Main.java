0ad614fa8da5dc1ad8459ceb4f756768dab8067b
Dan Pennebacker <dpennebacker@pvsd.org>
2018-11-27 20:09:38 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String t1 = cat(68, 108);
+        System.out.println(t1);
+        String t2 = cat(35, 59);
+        System.out.println(t2);
+    }
+
+    private static String cat(int start, int end)
+    {
+        //Code goes here
+    }
+}
